d0d2340d01a6e9a01de99ea88111b2efb46e9845
Noah Osterhout <osterh3@ad.nmc.edu>
2018-10-18 13:34:40 -0400

    Added project files

---
+++ Delegates/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class Program
+    {
+
+        delegate double Calculate(double operand1, double operand2);
+
+
+
+        static void Main(string[] args)
+        {
+            // Anonymous function / method
+            Calculate Multiply = delegate (double operand1, double operand2) { return operand1 * operand2; };
+
+            Calculate operation = Add;
+            DisplayAnswer(operation, 5, 7);
+
+            DisplayAnswer(Add, 5, 7);
+            DisplayAnswer(Subtract, 5, 7);
+            DisplayAnswer(Multiply, 5, 7);
+        }
+
+        static double Add(double operand1, double operand2)
+        {
+            return operand1 + operand2;
+        }
+
+        static double Subtract(double operand1, double operand2)
+        {
+            return operand1 - operand2;
+        }
+
+        static void DisplayAnswer(Calculate operation, double operand1, double operand2)
+        {
+
+            Console.WriteLine($"Answer: {operation(operand1, operand2)}");
+            Console.ReadKey();
+
+        }
+    }
+}
